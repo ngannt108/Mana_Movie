c6a2d6ecde0b496357c42200a966fb79270e93c0
vinhnam191 <vinhnam191@gmail.com>
2022-10-09 16:36:03 +0700

    Components 09/10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import './App.css';
-import Home from './Component/Home/Home'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Component/Header/Header";
+import Home from "./Component/Home/Home";
 
 export default function App() {
-  return ( 
-    <Home/>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
